b259aa2a20ae6cdfc22fc997ab8f0b601b028863
lostmart <martinpedraza1979@gmail.com>
2025-10-30 22:22:01 +0100

    contact form added

--- front-ang/src/app/app.routes.ts
+++ front-ang/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 
 import { Home } from './features/home/home';
+import { Support } from './features/support/support';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
+  },
+  {
+    path: 'support',
+    component: Support,
   },
   {
     path: '**',
