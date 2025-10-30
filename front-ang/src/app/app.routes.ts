da1b8443a18f832ee3db7d06815fef5011011a0d
lostmart <martinpedraza1979@gmail.com>
2025-10-30 22:10:56 +0100

    new additions

--- front-ang/src/app/app.routes.ts
+++ front-ang/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './features/home/home';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
